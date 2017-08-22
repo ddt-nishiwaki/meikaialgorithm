47517d7d23b63eb63dfc244436e82e55442bc6f6
Seiji Nishiwaki <ddt.jissyusei2@gmail.co>
2017-08-23 06:28:20 +0900

    演習1-2 100%

---
+++ kensyu02/src/kensyu02/OperateMin3.java
@@ -0,0 +1,51 @@
+package kensyu02;
+
+import java.util.Scanner;
+import static math.ExtremeOperation.*;
+
+//3つの数値から最小値を求めるメソッドを作成し実行するクラス
+public class OperateMin3 {
+	
+	// 数値入力を求めるメッセージを設定する
+	private static final String MESSAGE_REQUEST_COMPARE_VALUE = "入力した値から最小値を求めます。数値を入力してください : ";
+	// 一つ目の値入力を求めるメッセージを設定する
+	private static final String MESSAGE_REQUEST_FIRST_VALUE = "一つ目の値を設定してください";
+	// 二つ目の値入力を求めるメッセージを設定する
+	private static final String MESSAGE_REQUEST_SECOND_VALUE = "二つ目の値を設定してください";
+	// 三つ目の値入力を求めるメッセージを設定する
+	private static final String MESSAGE_REQUEST_THIRD_VALUE = "三つ目の値を設定してください";
+	// 最大値を出力する際のメッセージを設定する
+	private static final String MESSAGE_OUTPUT_MIN_VALUE = "入力した値の中から最小値を出力します";
+	
+	// 4つの数値から最小値を求めるメソッドを作成し実行するクラス
+	public static void main (String arg[]){
+		int firstValue;		// 一つ目の入力値を保持する変数を設定する
+		int secondValue;	// 二つ目の入力値を保持する変数を設定する
+		int thirdValue;		// 三つ目の入力値を保持する変数を設定する
+		
+		// 最小値を求める対象となる数値の入力を取得するオブジェクトを作成する
+		Scanner InputScanner = new Scanner(System.in);
+		// 最小値を求める対象となる数値の入力を求める
+		System.out.println(MESSAGE_REQUEST_COMPARE_VALUE);
+		// 一つ目の値の入力を求める
+		System.out.println(MESSAGE_REQUEST_FIRST_VALUE);
+		// 一つ目の値を保持する
+		firstValue = InputScanner.nextInt();
+		// 二つ目の値の入力を求める
+		System.out.println(MESSAGE_REQUEST_SECOND_VALUE);
+		// 二つ目の値を保持する
+		secondValue = InputScanner.nextInt();
+		// 三つ目の値の入力を求める
+		System.out.println(MESSAGE_REQUEST_THIRD_VALUE);
+		// 三つ目の値を保持する
+		thirdValue = InputScanner.nextInt();
+		// 最大値を出力する際のメッセージを出力する
+		System.out.println(MESSAGE_OUTPUT_MIN_VALUE);
+		// 四つの値から最小値を求めて出力します
+		System.out.println( min3(firstValue,secondValue,thirdValue) );
+		
+		// 入力用ストリームを閉じる
+		InputScanner.close();
+	}
+}
+
